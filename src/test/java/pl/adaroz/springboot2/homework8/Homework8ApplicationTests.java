b49274767d85db9a014808abfee274d8193e37f5
Adam Rozycki <adamrozycki00@gmail.com>
2020-04-04 20:06:01 +0200

    Initial commit

---
+++ src/test/java/pl/adaroz/springboot2/homework8/Homework8ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adaroz.springboot2.homework8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
