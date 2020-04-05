79328a7dfe272726de58e30d243b48525ddedc22
Adam Rozycki <adamrozycki00@gmail.com>
2020-04-05 14:06:04 +0200

    Immaterial refactoring

--- src/main/java/pl/adaroz/springboot2/homework8/weatherapi/apiservice/WeatherApiService.java
+++ src/main/java/pl/adaroz/springboot2/homework8/weatherapi/apiservice/WeatherApiService.java
@@ -8,12 +8,11 @@ import pl.adaroz.springboot2.homework8.weatherapi.apiclasses.WeatherInfo;
 public class WeatherApiService {
 
     public WeatherInfo getWeatherInfo(String cityParam) {
-        RestTemplate restTemplate = new RestTemplate();
         final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
         final String API_KEY_PARAM = "2c9ddfaf37b04cafa226704ab7a66458";
         final String UNITS_PARAM = "metric";
         String url = BASE_URL + "?q=" + cityParam + "&units=" + UNITS_PARAM + "&appid=" + API_KEY_PARAM;
-        return restTemplate.getForObject(url, WeatherInfo.class);
+        return new RestTemplate().getForObject(url, WeatherInfo.class);
     }
 
     public double getCurrentTempForCity(String city) {
