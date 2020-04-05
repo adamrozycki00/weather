7b3ccb0a0986b5ec681a8d68464ba737f8eef8fb
Adam Rozycki <adamrozycki00@gmail.com>
2020-04-05 09:40:34 +0200

    Model classes, method getWeatherInfo() in class WeatherService

---
+++ src/main/java/pl/adaroz/springboot2/homework8/model/Wind.java
@@ -0,0 +1,44 @@
+
+package pl.adaroz.springboot2.homework8.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "speed"
+})
+public class Wind {
+
+    @JsonProperty("speed")
+    private Integer speed;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("speed")
+    public Integer getSpeed() {
+        return speed;
+    }
+
+    @JsonProperty("speed")
+    public void setSpeed(Integer speed) {
+        this.speed = speed;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ src/main/java/pl/adaroz/springboot2/homework8/service/WeatherService.java
@@ -0,0 +1,20 @@
+package pl.adaroz.springboot2.homework8.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pl.adaroz.springboot2.homework8.model.WeatherInfo;
+
+@Service
+public class WeatherService {
+
+    public WeatherInfo getWeatherInfo(String cityParam) {
+        RestTemplate restTemplate = new RestTemplate();
+        final String API_KEY_PARAM = "07c0c3519429b51e18c2e2e2034b6dd3";
+        final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+        final String UNITS_PARAM = "metric";
+        String url = BASE_URL + "?q=" + cityParam + "&units=" + UNITS_PARAM + "&appid=" + API_KEY_PARAM;
+        WeatherInfo weatherInfo = restTemplate.getForObject(url, WeatherInfo.class);
+        return weatherInfo;
+    }
+
+}
